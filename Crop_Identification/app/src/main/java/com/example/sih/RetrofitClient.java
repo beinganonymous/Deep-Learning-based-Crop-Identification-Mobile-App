d7cce04494f43730e85466278a71be207b531449
Chetan Patil <chetanpatil49554@gmail.com>
2020-02-08 22:48:41 +0530

    Added REST API Support

---
+++ Crop_Identification/app/src/main/java/com/example/sih/RetrofitClient.java
@@ -0,0 +1,30 @@
+package com.example.sih;
+
+import android.content.Context;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit instance;
+    private static final String BASE_URL = "http://192.168.43.211:5002";
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofitClient(Context context) {
+
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+}
+
+
