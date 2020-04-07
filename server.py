28bf26299e82a68a03ee330849a3ca1422c4c6fb
Chetan Patil <chetanpatil49554@gmail.com>
2020-04-07 21:52:22 +0530

    Added sever.py

---
+++ server.py
@@ -0,0 +1,56 @@
+import aiohttp
+import asyncio
+import uvicorn
+import os
+from fastai import *
+from fastai.vision import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+
+export_file_name = '/home/shanks/Desktop/Image-Recognition/sasad/face5.pkl'
+
+classes = os.listdir('/home/shanks/Desktop/Image-Recognition/sasad/train')
+path = Path(__file__).parent
+path1 = '/home/shanks/Desktop/Image-Recognition/sasad'
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='/home/shanks/Desktop/Image-Recognition/app/static'))
+
+async def setup_learner():
+    try:
+        learn = load_learner(path1, export_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(setup_learner())]
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+@app.route('/')
+def index(request):
+    html = path/'view'/'index.html'
+    return HTMLResponse(html.open().read())
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    data = await request.form()
+    img_bytes = await (data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    prediction = learn.predict(img)[0]
+    return JSONResponse({'result': str(prediction)})
+
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=5002, log_level="info")
